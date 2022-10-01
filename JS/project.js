ba113cbadc1be5426e7c82f491d6fa3ebcd1e60d
Kyieng <koechconcepta@gmail.com>
2022-10-01 14:06:05 +0300

    assignment edited

--- JS/project.js
+++ JS/project.js
@@ -1,16 +1,13 @@
-function myFunction() {
-    document.getElementById("").innerHTML = "Paragraph changed.";
+function formRefresh(){
+    document.getElementById("getAkanName").reset();
   }
 
-const MALE_NAMES=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-const FEMALE_NAMES=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-const DAY_OF_THE_WEEK=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var dd = null;
-var mm = null;
-var yy = null;
-var gender = null;
-var cc = null;
-
+    var dd = null;
+    var mm = null;
+    var yy = null;
+    var gender=document.getElementById("gender").value;
+    var cc = null;
+  
 // get user input
    // get user birythday 22.12.1992
    // get user gender m,f
@@ -23,40 +20,48 @@ function getUserInput(){
      mm = prompt("Enter your month of birth:");
      yy = prompt("Enter your year of birth:");
      gender = prompt("Enter your gender: [m/f]:");
-     cc = yy.slice(0,2); 
+    
 
     }
+// formular for getting day of birth
 
+    const MALE_NAMES=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+    const FEMALE_NAMES=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+    const DAY_OF_THE_WEEK=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    
 // convert string to integer
 var dateOfBirth = document.getElementById("dateofbirth").value
-var day = parseInt(dd);
-var month = parseInt(mm);
-var year = parseInt(yy);
-var century = parseInt (cc);
+var day = parseInt(dd[2]);
+var month = parseInt(mm[1]);
+var year = parseInt(yy[0]);
 
-
-
-// formular for getting day of birth
+// day calulations
 function getAkanNames(day,month,year,gender){
+    var century=(year-1)/100+1; 
     var indexOfDay = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*((month+1)/10)) + day )) % 7
-    // Sunday
+    alert(indexOfDay);
     
 
-    var name = null;
-    
-    if (gender === "m"){
-        document.getElementById("display").innerHTML = "your Akan name is" + MALE_NAMES[indexOfDay];
-    } 
-    else if (gender === "f"){
-        document.getElementById("display").innerHTML = "your Akan Name is" + FEMALE_NAMES[indexOfDay]; 
-    }
-    else {
-        alert("invalid gender");
-        document.getElementById("display").innerHTML="";
-    }
+    var dayName = Math.round(indexOfDay)
 
+    // validation and result
+    if(!isNaN(dayName)){
+        if(gender=="m"){
+            document.getElementById("display").innerHTML= "Your akan names is"+ MALE_NAMES[Math.round(dayName)]+" and you were born on "+ DAY_OF_THE_WEEK[dayName];
+        }
+        else if(gender==="f"){
+            document.getElementById("display").innerHTML="You were born on "+ DAY_OF_THE_WEEK[dayName]+ " and Your akan name is " + FEMALE_NAMES[Math.round(dayName)];
+        }else{
+            alert("Enter valid dates and choose your gender");
+            document.getElementById("display").innerHTML="";
+        }
+    }
+            else{
+                alert("Enter valid Date of Birth and choose your gender");
+                document.getElementById("display").innerHTML="";
+            }
 }
 
-    var getAkanNames = document.getElementById("display").innerHTML;
+    document.getElementById("display").innerHTML = getAkanNames
     var userAkanName = getAkanNames(day,month,year,gender);
     console.log("Your name is" + userAkanName); 
